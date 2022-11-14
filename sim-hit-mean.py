1d61dae661fb0fef7962f63a36124f3ddebfdc3d
Nathan Barry <nathanbarry@protonmail.com>
2022-11-14 09:46:31 -0600

    fix tie misspell

--- sim-hit-mean.py
+++ sim-hit-mean.py
@@ -57,7 +57,7 @@ class HitExpectedStrategy:
         print("\n---FINAL OUTPUT---")
         print(f"Player Score: {self.playerWins}")
         print(f"Dealer Score: {self.dealerWins}")
-        print(f"Ties: {self.Ties}")
+        print(f"Ties: {self.tie}")
 
 
 b = HitExpectedStrategy()
